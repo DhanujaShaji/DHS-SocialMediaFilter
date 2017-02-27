ddf304fc769aaa56bf19edeef712c8efc6b6ae21
daiker0330 <daiker0330@gmail.com>
2017-02-26 19:50:44 -0500

    finish frontend prototype

--- Frontend/routes/index.js
+++ Frontend/routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('submit', { title: 'Submit Twitter Account' });
+});
+
+/* GET submit page. */
+router.get('/submit', function(req, res, next) {
+    res.render('submit', { title: 'Submit Twitter Account' });
+});
+
+/* GET report page. */
+router.get('/report', function(req, res, next) {
+    res.render('report', { title: 'Statistic Report' });
+});
+
+/* GET anonymized page. */
+router.get('/anonymized', function(req, res, next) {
+    res.render('anonymized', { title: 'Anonymized Twitter Data' });
+});
+
+/* GET original page. */
+router.get('/original', function(req, res, next) {
+    res.render('original', { title: 'Original Twitter Data:' });
+});
+
+/* GET decision page. */
+router.get('/decision', function(req, res, next) {
+    res.render('decision', { title: 'Make a Decision' });
 });
 
 module.exports = router;
