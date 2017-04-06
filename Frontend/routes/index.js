fdbf743c249d904f14590e9d918104d09095bd6c
daiker0330 <daiker0330@gmail.com>
2017-04-05 23:14:03 -0400

    update frontend

--- Frontend/routes/index.js
+++ Frontend/routes/index.js
@@ -5,52 +5,112 @@ var conn = null;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    // TODO: goto portal page if login
+
+    // goto login page if not login
     res.render('login', { title: 'Login Agent Account' });
 });
 
 /* GET login page. */
 router.post('/login', function (req, res, next) {
-    var email = req.body.emailAddress;
-    var psd = req.body.psd;
-    data = {"name": email};
-    res.render('portal', { title: 'Welcome ' + email, data: data });
+    var decision = req.body.action;
+    if (decision == 'login') {
+        var username = req.body.username;
+        var psd = req.body.psd;
+
+        // TODO: authentication
+
+        // TODO: get current user info
+        data = {"name": username};
+
+        res.render('portal', {title: 'Welcome ' + username, data: data});
+    } else if (decision == 'forgetPassword') {
+        // TODO: forget password
+    }
 });
 
 /* GET portal page*/
 router.get('/portal', function (req, res, next) {
     var decision = req.query.action;
     if (decision == "audit") {
-        res.render('submit', { title: 'Submit Twitter Account' });
+
+        // TODO: send current user identifier to distinguish different decision
+        auditData = [];
+
+        res.render('submit', { title: 'Submit Twitter Account', data: auditData });
     } else if (decision == "review") {
-        res.render('review', { title: 'Review Previous Decision' });
+
+        // TODO: add previous decisions to data
+        previousDecisions = [];
+
+        res.render('review', { title: 'Review Previous Decision', data: previousDecisions });
+    } else if (decision == 'passcode') {
+
+        // TODO: generate passcode (supervisor only)
+        data = {"passcode": "slopitjmgbhnkirot456rky"};
+
+        res.render('passcode', { title: 'Passcode' , data: data });
     } else {
-        data = {"name": "XXX"};
-        res.render('portal', { title: 'Welcome' , data: data });
+        // TODO: get current user info
+        data = {"name": 'user'};
+
+        res.render('portal', {title: 'Welcome ' + 'user', data: data});
+    }
+});
+
+router.get('/passcode', function (req, res, next) {
+    var decision = req.query.action;
+    if (decision == 'generate') {
+
+        // TODO: generate passcode (supervisor only)
+        data = {"passcode": "ap893rhdfaw893ey4htp2"};
+
+        res.render('passcode', { title: 'Passcode' , data: data });
+    } else if (decision == 'back') {
+
+        // TODO: get current user info
+        data = {"name": 'user'};
+
+        res.render('portal', {title: 'Welcome ' + 'user', data: data});
     }
 });
 
 /* GET review page */
 router.get('/review', function (req, res, next) {
+
+    // TODO: add previous decisions to data
+    previousDecisions = [];
+
     res.render('review', { title: 'Review Previous Decision' });
 });
 
 /* GET detail page */
 router.get('/detail', function (req, res, next) {
-    res.render('detail', { title: 'Decision Detail' });
+
+    // TODO: send decision detail info
+    decisionData = {};
+
+    res.render('detail', { title: 'Decision Detail', data: decisionData });
 });
 
 /* GET submit page. */
 router.get('/submit', function(req, res, next) {
+
+    // TODO: mockup data, need to replace with the real data from database
     data = [
         {'title': 'Flagged Tweets:', 'content': [
-            {'type': 'paragraph', 'content': 'Total Number: 123 Flagged Number: 23 Percentage: xx%'}
+            {'type': 'paragraph', 'content': '[num of flagged tweets] ([percentage]). || Average per traveler: [average number of flagged tweets]'},
+            {'type': 'paragraph', 'content': 'Ex). 120 (49%). || Average per traveler: 19'}
         ]},
-        {'title': 'Blacklist Followers:', 'content': [
-            {'type': 'paragraph', 'content': 'Total Number: 321 Flagged Number: 21 Percentage: xx%'}
+        {'title': 'Blacklisted Entities Following:', 'content': [
+            {'type': 'paragraph', 'content': '[num of blacklisted entities following] ([percentage])'},
+            {'type': 'paragraph', 'content': 'Ex). 30 (12%)'}
         ]},
         {'title': 'Other Statistic Report:', 'content': [
             {'type': 'paragraph', 'content': '......'}
         ]}];
+
     res.render('report', { title: 'Statistic Report', data: data });
 });
 
@@ -58,32 +118,38 @@ router.get('/submit', function(req, res, next) {
 router.get('/report', function(req, res, next) {
     var decision = req.query.action;
     if (decision == "no") {
+
+        // TODO: mockup data, need to replace with the real data from database
         data = [
             {'type': 'danger', 'date': '2012-12-21', 'comment': '(flagged)', 'content': [
-                {'type': 'text', 'content': '2012 will'},
-                {'type': 'keyword', 'content': 'destroy', 'level': 'danger', 'reason-title': 'Sensitive Type',
+                {'type': 'text', 'content': 'Cookies are bad I will '},
+                {'type': 'keyword', 'content': 'kill ', 'level': 'danger', 'reason-title': 'Sensitive Type',
                     'reason-content': 'Dangerous Keyword'},
-                {'type': 'text', 'content': 'the world!!!'},
-                {'type': 'at', 'content': '@***'},
-                {'type': 'tag', 'content': '#***'}
-            ]},
-            {'type': 'warning', 'date': '2012-12-21', 'comment': '(uncertain)', 'content': [
-                {'type': 'text', 'content': "The world hasn\'t been"},
-                {'type': 'keyword', 'content': 'destroyed', 'level': 'warning', 'reason-title': 'Sensitive Type',
-                    'reason-content': 'Dangerous Keyword'},
-                {'type': 'text', 'content': '!!!'},
-                {'type': 'at', 'content': '@***'},
-                {'type': 'tag', 'content': '#***'}
+                {'type': 'text', 'content': 'them. '},
+                {'type': 'at', 'content': '@user'},
+                {'type': 'tag', 'content': '#tag'}
             ]},
             {'type': 'success', 'date': '2012-12-22', 'comment': '(not flagged)', 'content': [
-                {'type': 'text', 'content': 'Hello World :)'},
-                {'type': 'at', 'content': '@***'},
-                {'type': 'tag', 'content': '#***'}
+                {'type': 'text', 'content': 'I had '},
+                {'type': 'keyword', 'content': '***** ', 'level': 'warning', 'reason-title': 'Anonymized Info',
+                    'reason-content': 'censors personal info'},
+                {'type': 'at', 'content': '@user'},
+                {'type': 'tag', 'content': '#tag'}
+            ]},
+            {'type': 'success', 'date': '2012-12-22', 'comment': '(not flagged)', 'content': [
+                {'type': 'text', 'content': 'I like pie. \n'},
+                {'type': 'at', 'content': '@user'},
+                {'type': 'tag', 'content': '#tag'}
             ]}
         ];
+
         res.render('anonymized', { title: 'Anonymized Twitter Data', data: data });
     } else {
-        res.render('decision', { title: 'Make a Decision' });
+
+        // TODO: add some info to make a decision
+        data = [];
+
+        res.render('decision', { title: 'Make a Decision', data: data });
     }
 });
 
@@ -91,6 +157,8 @@ router.get('/report', function(req, res, next) {
 router.get('/anonymized', function(req, res, next) {
     var decision = req.query.action;
     if (decision == "no") {
+
+        // TODO: mockup data, need to replace with the real data from database
         data = [
             {'type': 'danger', 'date': '2012-12-21', 'comment': '(flagged)', 'content': [
                 {'type': 'text', 'content': '2012 will'},
@@ -100,28 +168,29 @@ router.get('/anonymized', function(req, res, next) {
                 {'type': 'at', 'content': '@someone'},
                 {'type': 'tag', 'content': '#sometag'}
             ]},
-            {'type': 'warning', 'date': '2012-12-21', 'comment': '(uncertain)', 'content': [
-                {'type': 'text', 'content': "The world hasn\'t been"},
-                {'type': 'keyword', 'content': 'destroyed', 'level': 'warning', 'reason-title': 'Sensitive Type',
-                    'reason-content': 'Dangerous Keyword'},
-                {'type': 'text', 'content': '!!!'},
-                {'type': 'at', 'content': '@someone'},
-                {'type': 'tag', 'content': '#sometag'}
-            ]},
             {'type': 'success', 'date': '2012-12-22', 'comment': '(not flagged)', 'content': [
-                {'type': 'text', 'content': 'Hello World :)'},
+                {'type': 'text', 'content': 'I had sex with her tonight'},
                 {'type': 'at', 'content': '@someone'},
                 {'type': 'tag', 'content': '#sometag'}
             ]}
         ];
+
         res.render('original', { title: 'Original Twitter Data:', data: data });
     } else {
+
+        // TODO: add some info to make a decision
+        data = [];
+
         res.render('decision', { title: 'Make a Decision' });
     }
 });
 
 /* GET original page. */
 router.get('/original', function(req, res, next) {
+
+    // TODO: add some info to make a decision
+    data = [];
+
     res.render('decision', { title: 'Make a Decision' });
 });
 
@@ -129,9 +198,9 @@ router.get('/original', function(req, res, next) {
 router.get('/decision', function(req, res, next) {
     var decision = req.query.action;
     if (decision == "yes") {
-        ;
+        // TODO: pass decision
     } else {
-        ;
+        // TODO: deny decision
     }
     res.render('submit', { title: 'Submit Twitter Account' });
 });
